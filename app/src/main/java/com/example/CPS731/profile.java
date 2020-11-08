675142661ed03b2bea909914e1349017c421d316
kirtan1312 <kirtan.m.patel@ryerson.ca>
2020-11-08 16:46:31 -0500

    Test

---
+++ app/src/main/java/com/example/CPS731/profile.java
@@ -0,0 +1,34 @@
+package com.example.CPS731;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+       // myDB = new DataBaseHelper(this);
+
+        //set listener for button
+
+
+    }
+
+
+
+}
