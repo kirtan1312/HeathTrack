e60e14652a11f9de806851276a0e49d26250eee4
kirtan1312 <kirtan.m.patel@ryerson.ca>
2020-11-29 00:31:20 -0500

    Test

---
+++ app/src/main/java/com/example/CPS731/splash.java
@@ -0,0 +1,27 @@
+package com.example.CPS731;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        };
+        Timer t = new Timer();
+        t.schedule(task,5000);
+    }
+}
